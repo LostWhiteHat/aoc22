90a0bc7e044532d8f38e6ddb6b46b2870855ba6a
Lost <patrick.buerdel@edu.band.ch>
2023-10-09 14:37:21 +0200

    update

--- 2/main.c
+++ 2/main.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum State
-{
-    ROCK,
-    PAPER,
-    SCISSORS,
-    WIN,
-    LOSE,
-    DRAW,
-    NONE
-};
-
 struct Round
 {
     char enemy;
@@ -19,16 +8,17 @@ struct Round
     int points;
 };
 
-int main()
+void part_one()
 {
     FILE *fptr;
     char *line = NULL;
     size_t len = 0;
     size_t read;
 
-    struct Round round[2000];
-    struct Round roundElement;
-    struct Round resetRound;
+    struct Round round[2500];
+    struct Round round_element;
+    struct Round reset_round;
+    int round_counter = 0;
 
     fptr = fopen("./input.txt", "r");
     if (fptr == NULL)
@@ -36,9 +26,95 @@ int main()
 
     while ((read = getline(&line, &len, fptr)) != -1)
     {
-        roundElement.enemy = line[0];
-        roundElement.me = line[2];
-        }
+        round_element.enemy = line[0];
+        round_element.me = line[2];
+        switch (round_element.enemy)
+        {
+        case 'A':
+            switch (round_element.me)
+            {
+            case 'X':
+                round_element.points += 4;
+                break;
+            case 'Y':
+                round_element.points += 8;
+                break;
+            case 'Z':
+                round_element.points += 3;
+                break;
+            }
+            break;
 
+        case 'B':
+            switch (round_element.me)
+            {
+            case 'X':
+                round_element.points += 1;
+                break;
+            case 'Y':
+                round_element.points += 5;
+                break;
+            case 'Z':
+                round_element.points += 9;
+                break;
+            }
+            break;
+
+        case 'C':
+            switch (round_element.me)
+            {
+            case 'X':
+                round_element.points += 7;
+                break;
+            case 'Y':
+                round_element.points += 2;
+                break;
+            case 'Z':
+                round_element.points += 6;
+                break;
+            }
+            break;
+        }
+        round[round_counter] = round_element;
+        round_counter++;
+        round_element = reset_round;
+    }
+
+    int total = 0;
+    for (int i = 0; i < 2500; i++)
+    {
+        total += round[i].points;
+    }
+    printf("Part 1: %d\n", total);
+    fclose(fptr);
+    if (line)
+        free(line);
+}
+
+void part_two()
+{
+    FILE *fptr;
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+
+    struct Round round[2500];
+    struct Round round_element;
+    struct Round reset_round;
+    int round_counter = 0;
+
+    fptr = fopen("./input.txt", "r");
+    if (fptr == NULL)
+        exit(EXIT_FAILURE);
+
+    fclose(fptr);
+    if (line)
+        free(line);
+}
+
+int main()
+{
+    part_one();
+    part_two();
     exit(EXIT_SUCCESS);
 }
